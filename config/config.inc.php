3bd8444eaebbe20fc8d88f3e5eaba35dfc76df9e
guswill1224 <121774232+guswill1224@users.noreply.github.com>
2025-08-26 20:42:25 +0000

    Subir

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,7 +1,9 @@
 <?php
+
 # If you are having problems connecting to the MySQL database and all of the variables below are correct
 # try changing the 'db_server' variable from localhost to 127.0.0.1. Fixes a problem due to sockets.
 #   Thanks to @digininja for the fix.
+
 # Database management system to use
 $DBMS = getenv('DBMS') ?: 'MySQL';
 #$DBMS = 'PGSQL'; // Currently disabled
@@ -50,3 +52,6 @@ define ('SQLITE', 'sqlite');
 $_DVWA['SQLI_DB'] = getenv('SQLI_DB') ?: MYSQL;
 #$_DVWA['SQLI_DB'] = SQLITE;
 #$_DVWA['SQLITE_DB'] = 'sqli.db';
+
+?>
+
