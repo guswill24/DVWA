1719f426507b5f9827ad3a1da6fd37d1f78281a1
guswill1224 <121774232+guswill1224@users.noreply.github.com>
2025-08-26 20:30:33 +0000

    subir

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,9 +1,7 @@
 <?php
-
 # If you are having problems connecting to the MySQL database and all of the variables below are correct
 # try changing the 'db_server' variable from localhost to 127.0.0.1. Fixes a problem due to sockets.
 #   Thanks to @digininja for the fix.
-
 # Database management system to use
 $DBMS = getenv('DBMS') ?: 'MySQL';
 #$DBMS = 'PGSQL'; // Currently disabled
